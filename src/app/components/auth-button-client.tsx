d1a52df4c0b427625c8f263cf67851ae31c0e98c
Piero010405 <84740254+Piero010405@users.noreply.github.com>
2024-04-01 19:13:11 -0500

    route fixed 1

--- src/app/components/auth-button-client.tsx
+++ src/app/components/auth-button-client.tsx
@@ -12,7 +12,7 @@ export function AuthButton({ session }: { session: Session | null }) {
         await supabase.auth.signInWithOAuth({
             provider: 'github',
             options: {
-                redirectTo: 'http://localhost:3000/auth/callback' // <-- lo vamos a redirigir a un endpoint de nuestra aplicacion
+                redirectTo: '/auth/callback' // <-- lo vamos a redirigir a un endpoint de nuestra aplicacion
             }
         })
     }
